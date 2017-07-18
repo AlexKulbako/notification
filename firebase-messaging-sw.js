94c7fcbfc2cccb132426e422d6d078ea4d52f762
Oleksii Kulbako <okulbako@gloriumtech.com>
2017-07-18 17:44:39 +0300

    jlkjl

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
-firebase.initializeApp({
-    'messagingSenderId': '448358493027'
-});
-
 firebase.messaging();
 
 self.addEventListener('notificationclick', function(event) {
